bca6d0d740a60c70c01ed7c8ba034bbaa8360c2b
Spencer Florence <spencer@florence.io>
2019-11-14 14:16:54 -0600

    add main package

---
+++ diagrama/info.rkt
@@ -0,0 +1,14 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps
+  '(("base" #:version "7.4")
+    "diagrama-lib"
+    "diagrama-doc"))
+
+(define build-deps '())
+
+(define pkg-desc "Implementation and Documentation part of `diagrama`")
+
+(define version "0.1")
